0466362c6246f92a15900c8c9adfc1fccfa341a9
igoharaujo <117318583+igoharaujo@users.noreply.github.com>
2023-05-11 11:55:59 -0300

    alter

--- 04-Exercicios/05-exercicio_function.sql
+++ 04-Exercicios/05-exercicio_function.sql
@@ -31,8 +31,10 @@ Crie uma função que deixe todos os caracteres de uma string minusculas.
 Crie uma função que ao ser chamada receba o codigo de um pai e me motra seus dados e quais são seus filhos.
 
 */
-
+-- ---------------------------------------------------------------------------------------------------------------------
+#Usando o db_discoteca2:
 use db_discoteca2;
+-- ---------------------------------------------------------------------------------------------------------------------
 
 -- 1- vw_gravadora - deve conter id, nome e total de discos
 
@@ -42,7 +44,7 @@ FROM tb_gravadora as gravadora left join tb_disco as disco
 on gravadora.id = disco.id_gravadora
 group by disco.id;
 
-#Correto: 
+#CORRIGIDO: 
 CREATE VIEW vw_gravadora2 as
 select gravadora.id as id, gravadora.nome as gravadora, count(disco.id_gravadora) as 'qtd de discos por gravadora' 
 FROM tb_gravadora as gravadora inner join tb_disco as disco
@@ -72,10 +74,16 @@ inner join tb_genero as genero
 SELECT * FROM vw_disco;
 
 -- 3 - vw_artista - deve conter id, nome completo, idade, total de discos
-CREATE VIEW vw_artista as
+
 SELECT artista.id, artista.nome as artista, timestampdiff(year, artista.dt_nascimento, curdate()) as idade, count(disco.id) as 'total disco'
 FROM tb_artista as artista inner join tb_disco as disco on artista.id = disco.id_artista
 group by disco.id;
+
+#CORRIGIDO
+create view vw_artista1 as
+SELECT artista.id, artista.nome as artista, timestampdiff(year, artista.dt_nascimento, curdate()) as idade, count(disco.id_artista) as 'total disco'
+FROM tb_artista as artista inner join tb_disco as disco on artista.id = disco.id_artista
+group by disco.id_artista;
 
 
 -- 4 - vw_musicas - deve conter nome da musica, duração, o disco e o artista.
@@ -88,23 +96,114 @@ ON artista.id = disco.id_artista;
 
 -- 5 - Crie uma function que ao receber o codigo de um disco nos diga qual o tempo total desse disco Usando a tabela musica.
 
+
 DELIMITER $$
- CREATE FUNCTION IF NOT EXISTS fn_tempo(cod_musica int)
+ CREATE FUNCTION IF NOT EXISTS fn_tempo(cod_disco int)
  RETURNS time
  DETERMINISTIC
 		BEGIN
 		DECLARE tempo TIME;
 		SET tempo = (select sec_to_time(sum(time_to_sec(duracao)))
 			from tb_musica
-			where id_disco = cod_musica);
+			where id_disco = cod_disco);
             
  RETURN tempo;
 		END $$
  DELIMITER ;
 
-select fn_tempo(1);
+select fn_tempo(7);
 
-drop function fn_tempo;
+-- 6 - Crie uma function que ao receber o codigo de um disco nos diga todas as informações desse disco.
+
+DELIMITER $$
+CREATE FUNCTION IF NOT EXISTS fn_info_disco(cod_disco INT)
+RETURNS VARCHAR(255)
+DETERMINISTIC
+	BEGIN
+		DECLARE info VARCHAR(255);
+		SET info = (select concat('id = ', id, ' - titulo = ', titulo, ' - duracao = ', duracao, ' - lançamento = ', ano_lancamento)  #'id do artista = ', id_artista, 'id da gravadora = ', id_gravadora, 'id do genero'
+		from tb_disco 
+        where id = cod_disco);
+    
+RETURN info;
+	END $$
+DELIMITER ;
+
+SELECT fn_info_disco(7);
+
+-- ---------------------------------------------------------------------------------------------------------------------
+#Usando o db_familia:
+use db_familia;
+-- ---------------------------------------------------------------------------------------------------------------------
+
+/*7.1 -  crie uma view que mostre o id e nome do filho, nome do pai e nome da mae, porém somente dos que estão relacionados. 
+Apos criada a view utiliezea em uma function que me premiter visualizar esses dados de um filho expecifico.*/
+
+CREATE VIEW vw_filhos as 
+select filho.id as id_filho, filho.nome as nome_filho, pai.nome as nome_pai , mae.nome as nome_mae
+from tb_filho as filho INNER join tb_mae as mae
+ON mae.id = filho.id_mae
+INNER JOIN tb_pai AS pai
+ON pai.id = filho.id_pai;
+
+select * from vw_filhos;
+#---------------------------------
+#OBS: para mostrar o view em uma function precisammos colocar o que queremos que seja exbido usando os apelidos que criamos no view
+DELIMITER $$
+CREATE FUNCTION IF NOT EXISTS fn_info_filhos(cod_filho INT)
+RETURNS varchar(255)
+deterministic 
+	begin
+		declare info varchar(255);
+        SET info = (select concat('id filho: ', id_filho,' - nome filho: ',nome_filho, ' - nome pai: ', nome_pai,' - nome mae: ', nome_mae) from vw_filhos
+        where id_filho = cod_filho); -- id_ filho é o apelido que criamos em vw_filho
+        
+    return info;
+    
+    end $$
+DELIMITER ;
+
+SELECT fn_info_filhos(19);
+
+
+/*8.2 - Ainda usando o db_familia, crie uma view que mostre o id e nome dos pais, o nome do filho, desejasse visualisar todos os pais, mesmo os que não possuem filhos. 
+crie uma functions que me permita visualizar esses dados pelo nome do pai.*/
+CREATE VIEW vw_pais as
+select pai.id as id_pai, pai.nome as nome_pai, mae.id as id_mae, mae.nome as nome_mae, filho.nome as nome_filho 
+from tb_pai as pai left join tb_filho as filho
+ON pai.id = filho.id_pai
+right join tb_mae as mae
+on mae.id = filho.id_mae;
+
+DELIMITER $$
+CREATE FUNCTION IF NOT EXISTS fn_pais(cod_pai int)
+RETURNS VARCHAR(255)
+DETERMINISTIC 
+	BEGIN
+		DECLARE info VARCHAR(255);
+        SET info = (select concat('id_pai: ', id_pai , ' | pai: ', nome_pai,' | id_mae: ', id_mae, ' | mae: ', nome_mae,' | filho: ', nome_filho) as 'informações familia'
+        from vw_pais 
+        where id_pai = cod_pai);
+    
+	RETURN info;
+	END $$
+DELIMITER ;
+
+select fn_pais(2);
+
+drop function fn_pais;
+
+
+
+-- 9.3 - Usando o db_discoteca, crie uma view que me permita visualisar todos os dados do disco, junto ao nome da gravadora e nome do artista.
+
+
+
+
+
+
+
+
 
 
 
