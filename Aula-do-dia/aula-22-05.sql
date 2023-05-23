4e6adaf34d44914e7b82729a824fd8bc02fff623
igoharaujo <117318583+igoharaujo@users.noreply.github.com>
2023-05-23 11:46:15 -0300

    alter

--- Aula-do-dia/aula-22-05.sql
+++ Aula-do-dia/aula-22-05.sql
@@ -81,9 +81,6 @@ ADD NOVO_NOME VARCHAR(255);
 
 
 
-
-
-
 drop trigger tr_delete_pai;
 
 DELIMITER $$
@@ -102,8 +99,168 @@ FOR EACH ROW     #para cada linha, ou: para cada registro da tabela varificamos 
 DELIMITER ;
 
 
-
 delete from tb_pai where id = 1;
+
+
+#-------------------------------------------------------------------------------23-05-20223
+create database if not exists db_loja
+COLLATE utf8mb4_general_ci
+CHARSET utf8mb4;
+
+use db_loja;
+
+
+CREATE TABLE IF NOT EXISTS tb_produto(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    preco FLOAT,
+    quantidade INT
+);
+
+CREATE TABLE IF NOT EXISTS tb_venda(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    id_produto INT NOT NULL,
+    qtd_comprada INT NOT NULL,
+    CONSTRAINT fk_id_produto FOREIGN KEY (id_produto) REFERENCES tb_produto(id));
+
+CREATE TABLE IF NOT EXISTS tb_extorno(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    id_venda INT NOT NULL,
+    qtd_extornada INT NOT NULL,
+    CONSTRAINT fk_id_venda FOREIGN KEY (id_venda) REFERENCES tb_venda(id));
+
+
+INSERT INTO tb_produto
+	(nome, preco, quantidade)
+VALUES
+	('iphone', '14.999', 55),
+    ('Playstation 5', '4999.99',100),
+    ('Xbox x', '4680.99', 10);
+    
+    update tb_produto set quantidade = 50 where id = 1;
+    
+    
+    #correção atualizadno a quantidade do produto de acordo com as vendas feitas
+DELIMITER $$
+CREATE TRIGGER tr_venda
+after insert
+ON tb_venda
+FOR EACH ROW
+
+	BEGIN
+		UPDATE tb_produto SET quantidade = (quantidade - new.qtd_comprada) WHERE id = new.id_produto;
+
+    END $$
+     
+DELIMITER ;
+
+
+
+     SELECT * FROM TB_PRODUTO;
+     
+INSERT INTO tb_venda
+	(id_produto, qtd_comprada)
+VALUES
+	(1,4);
+    
+    #----------------- EXTORNO DA COMPRA
+    
+    DELIMITER $$
+    CREATE TRIGGER tr_delete_atualiza_estoque
+    after delete
+    ON tb_venda
+    fOR each row 
+    BEGIN 
+		update TB_PRODUTO set quantidade = (quantidade + OLD.qtd_comprada)
+        where ID = ld.ID_PRODUTO;
+    
+
+    END $$
+    DELIMITER ;
+    DELETE FROM tb_venda WHERE id = 1;
+    
+    #----------------- UPDATE
+    
+    DELIMITER $$
+    CREATE TRIGGER tr_atualiza_estoque
+    after UPDATE
+    ON tb_venda
+    fOR each row 
+    BEGIN 
+		update TB_PRODUTO set quantidade = (quantidade + (OLD.qtd_comprada - NEW.qtd_comprada))
+        where ID = new.ID_PRODUTO;
+    
+
+    END $$
+    DELIMITER ;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    drop trigger tr_extorno;
+select * from tb_produto;
+     
+
+     
+     
+
+     
+     
+     
+     
+     
+     
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
