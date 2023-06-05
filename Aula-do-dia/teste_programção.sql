562c57e6051e0ff9c27f26bc6ab8502fe2616b9d
igoharaujo <117318583+igoharaujo@users.noreply.github.com>
2023-06-05 14:51:49 -0300

    ALTER

--- Aula-do-dia/teste_programção.sql
+++ Aula-do-dia/teste_programção.sql
@@ -2,7 +2,7 @@
 
 
 CREATE DATABASE IF NOT EXISTS restaurante -- Criando uma database chamada restaurante
-COLLATE utf8mb4_general_ci -- Tipo de texto usando
+COLLATE utf8mb4_general_ci 
 CHARSET utf8mb4;
 
 
@@ -10,3 +10,10 @@ CREATE TABLE IF NOT EXISTS produto( -- Criando a tabela produto
 	cod_produto INT PRIMARY KEY,
     nome VARCHAR(45),
     valor FLOAT);
+    
+    
+CREATE TABLE IF NOT EXISTS cliente(
+	cod_cliente INT PRIMARY KEY,
+    nome VARCHAR(45));
+    
+    CREATE table IFRT54      
